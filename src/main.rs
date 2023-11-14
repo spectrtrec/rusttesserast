052caf1e5299a44cefd2a904e52686882429ccf8
user <user>
2023-11-14 18:52:38 +0000

    Format documents.

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
 use rusttesserast::tess_lib::TesseractApi;
 
 fn main() {
-    let mut tesseract_base = TesseractApi::new(Some(String::from("/usr/local/share/tessdata").as_str()), Some(String::from("rus").as_str())).unwrap();
+    //let mut tesseract_base = TesseractApi{dpi: 3, psm:3, oem:3, ..Default::default()};
+    let mut tesseract_base = TesseractApi::new(Some(TesseractApi{dpi: 3, psm:3, ..Default::default()}), Some(String::from("/usr/local/share/tessdata").as_str()), Some(String::from("rus").as_str())).unwrap();
     let image_array = vec!["/workspaces/rusttesserast/tests/resipients.png"];
     let test = tesseract_base.recognize_doc(None, None, image_array, "tsv");
     println!("{:?}", test);
